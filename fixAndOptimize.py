4b9817488f2c9492da18b1d968e586af2195dab1
AndreyVLD <A.V.Nicula-1@student.tudelft.nl>
2024-10-10 18:10:48 +0200

    Improvements

--- fixAndOptimize.py
+++ fixAndOptimize.py
@@ -1,7 +1,10 @@
+import random
 import subprocess
+import time
 from itertools import combinations
 import math
 from pysat.formula import WCNF
+
 
 def n_choose_k(n, k):
     """
@@ -17,21 +20,23 @@ def n_choose_k(n, k):
     if k > n or n < 0 or k < 0:
         return 0
     return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
-def evaluate(x,timeout):
+
+
+def evaluate(x, timeout):
     # Define the path to the instance file
-    path_to_instance = r"C:\Users\Traian\Documents\GitHub\Timetabling-problem\encoding.dimacs"
-    wcnf=WCNF(path_to_instance)
+    path_to_instance = r"encoding.dimacs"
+    wcnf = WCNF(path_to_instance)
     for v in x:
         wcnf.append([v])
 
-    path_to_formula=r"..\..\Documents\GitHub\FixAndOptimizeTimetabling\current_formulab1.wcnf"
+    path_to_formula = r"C:\Projects\Honours\FixAndOptimizeTimetabling\current_formulab1.wcnf"
     with open("current_formulab1.wcnf", 'w') as file:
         # Write the header line
         file.write(wcnf.to_dimacs())
 
     # " | findstr /R /C:\"v..\(.*\)\"" +
     print("era lenx")
-    command = r"cd ..\..\Pumpkin\pumpkin-private && target\release\pumpkin-cli.exe -t " + str(timeout) + " " + path_to_formula + " > " + r"..\..\PycharmProjects\tentativa\capture_sol.txt"
+    command = r"pumpkin-cli.exe -t " + str(timeout) + " " + path_to_formula + " > " + r"capture_sol.txt"
 
     starting_directory = r"C:"
 
@@ -39,7 +44,7 @@ def evaluate(x,timeout):
     process.wait()
     last_o_number = 1000000000
     print("done")
-    output=[]
+    output = []
     with open("capture_sol.txt", "r") as file:
         for line in file:
             # Check if the line starts with 'o'
@@ -53,6 +58,8 @@ def evaluate(x,timeout):
                     # Handle the case where the element is not found or not an integer
                     print(f"Skipping invalid 'o' line: {line.strip()}")
     return last_o_number
+
+
 def read_numbers_from_file(file_path="capture_sol.txt"):
     numbers = []
     try:
@@ -67,11 +74,13 @@ def read_numbers_from_file(file_path="capture_sol.txt"):
     except ValueError:
         print("File contains non-numeric data.")
     return numbers
+
+
 def read_file_with_signs(file_path):
-    dict={}
+    dict = {}
     with open(file_path, 'r') as file:
         for line in file:
-        # Read the first line from the file
+            # Read the first line from the file
             linee = line.strip()
             #print(linee)
             # Check if the line starts with 'v'
@@ -80,72 +89,92 @@ def read_file_with_signs(file_path):
                 numbers_str = line.split()[1:]
 
                 # Convert the remaining elements to integers
-                for num  in numbers_str:
-                    dict[abs(int(num))]=int(num)
-                return dict
-k=1
-solution=read_numbers_from_file("sol.txt")
-initnr=len(solution)
-no_change=0
-ind_perm=0
-ind=0
+                for num in numbers_str:
+                    dict[abs(int(num))] = int(num)
+    return dict
+
+def variable_dict(file_path):
+    dict = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            curr_line = line.strip()
+            nums = line.split()[1:]
+            for num in nums:
+                dict[abs(int(num))] = int(num)
+    return dict
+
+
+solution = read_numbers_from_file("sol.txt")
+initnr = len(solution)
+no_change = 0
+ind_perm = 0
+ind = 0
+dict = variable_dict("sol.txt")
+print(dict)
 varToFix = read_numbers_from_file("variableToFixOn.txt")
-arr=range(int(len(varToFix)))
-subsets=[list(combination) for combination in combinations(arr, k)]
+print(varToFix)
+k = int(len(varToFix)*5/10)
+arr = range(int(len(varToFix)))
+# subsets = [list(combination) for combination in combinations(arr, k)]
 #print(solution)
-cost=evaluate(solution,100000)
+cost = evaluate(solution, 100000)
 #print(len(solution))
 print(cost)
+random.seed(time.time())
+# for combination in combinations(arr, k):
+#     print(combination)
+while k <= len(arr):
 
-while k<=len(arr):
-    print((ind_perm))
-    print(no_change)
-    arr=subsets[ind_perm]
+    # print((ind_perm))
+    #print(no_change)
+
+    arr = random.sample(varToFix, k)
+    print(arr)
     #print(subsets)
-    curr_sol=[]
-    ind=0
-    #print(arr)
+    curr_sol = []
+    ind = 0
+    # print(arr)
     for a in arr:
-        curr_sol.append(solution[varToFix[a]])
-        print(varToFix[a])
-#     for i in range(int(initnr/25)):
-#         if i not in arr:
-#             for j in range(25):
-# #                 print(len(solution))
-# #                 print(ind)
-# #                 print(solution[ind])
-#                 curr_sol.append(solution[varToFix[ind]])
-#                 ind+=1
-#         else:
-#             ind+=25
+        curr_sol.append(dict[a])
+        # print("Var to fix: ", a)
+    #     for i in range(int(initnr/25)):
+    #         if i not in arr:
+    #             for j in range(25):
+    # #                 print(len(solution))
+    # #                 print(ind)
+    # #                 print(solution[ind])
+    #                 curr_sol.append(solution[varToFix[ind]])
+    #                 ind+=1
+    #         else:
+    #             ind+=25
     #print(curr_sol)
     with open("solution_memory.txt", 'w') as f:
         for item in curr_sol:
             f.write(str(item) + ' ')
-    curr_cost=evaluate(curr_sol,20000)
-    print(cost)
-    if curr_cost<cost:
-        cost=curr_cost
-        ind=0
-        dict=read_file_with_signs("capture_sol.txt")
+    curr_cost = evaluate(curr_sol, 60000)
+    print(cost, " the current cost is: ", curr_cost)
+    if curr_cost < cost:
+        cost = curr_cost
+        ind = 0
+        dict = read_file_with_signs("capture_sol.txt")
         with open("best assignment.txt", 'w') as f:
             for item in curr_sol:
                 f.write(str(item) + ' ')
         #print(dict)
-        curr_sol=[]
+        curr_sol = []
         for x in solution:
             curr_sol.append(dict[abs(x)])
-        solution=curr_sol
+        solution = curr_sol
         #print(solution)
-        no_change=0
-        ind_perm=-1
+        no_change = 0
+        ind_perm = -1
     else:
-        no_change+=1
-    if no_change==n_choose_k(int(initnr/25), k):
-        k+=1
-        arr=range(int(len(solution)/25))
-        subsets=[list(combination) for combination in combinations(arr, k)]
-        ind_perm=0
-        no_change=0
+        no_change += 1
+    if no_change == n_choose_k(int(initnr / 25), k):
+        k += 1
+        arr = range(int(len(solution) / 25))
+        subsets = [list(combination) for combination in combinations(arr, k)]
+        ind_perm = 0
+        no_change = 0
     else:
-        ind_perm+=1
+        ind_perm += 1
